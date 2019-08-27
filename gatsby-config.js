8eb76a40cefdeeb9c6c4cbc7f549e714a1132e2b
fpunny <frederic.pun@gmail.com>
2019-08-27 02:12:08 -0400

    Removed stapi

--- gatsby-config.js
+++ gatsby-config.js
@@ -74,14 +74,14 @@ module.exports = {
         production: true
       },
     },
-    {
+    /* {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: `http://localhost:1337`,
         queryLimit: 1000, // Default to 100
         contentTypes: [`article`, `user`, `division`],
       },
-    },
+    }, */
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
